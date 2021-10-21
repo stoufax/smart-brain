815556d589680a06b350e3411d766079698bf836
Mustafa Mokrani <mustafamo@xtech.guru>
2021-10-21 22:14:30 +0100

    minor change.

--- packages/smart-brain-api/server.js
+++ packages/smart-brain-api/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const knex = require('knex');
 const morgan = require('morgan');
-
-require('dotenv').config();
 
 const { signinAuthentication } = require('./controllers/signin');
 const { handleRegister } = require('./controllers/register');
